a28e45a8e4022b7ccf74b887ea2de0b8150794c4
job <bobrik717@gmail.com>
2018-10-09 12:46:56 +0300

    first commit

---
+++ app/components/ActionButtons.vue
@@ -0,0 +1,28 @@
+<template>
+    <FlexboxLayout flexWrap="wrap">
+        <Button text="1" width="25%" @tap="onButtonTap(1)" />
+        <Button text="2" width="25%" @tap="onButtonTap(2)" />
+        <Button text="3" width="25%" @tap="onButtonTap(3)" />
+        <Button text="-" width="25%" @tap="onButtonTap('-')" />
+        <Button text="4" width="25%" @tap="onButtonTap(4)" />
+        <Button text="5" width="25%" @tap="onButtonTap(5)" />
+        <Button text="6" width="25%" @tap="onButtonTap(6)" />
+        <Button text="+" width="25%" @tap="onButtonTap('+')" />
+        <Button text="7" width="25%" @tap="onButtonTap(7)" />
+        <Button text="8" width="25%" @tap="onButtonTap(8)" />
+        <Button text="9" width="25%" @tap="onButtonTap(9)" />
+        <Button text="*" width="25%" @tap="onButtonTap('*')" />
+        <Button text="0" width="75%" @tap="onButtonTap(0)" />
+        <Button text="/" width="25%" @tap="onButtonTap('/')" />
+    </FlexboxLayout>
+</template>
+
+<script>
+    export default {
+        name: "ActionButtons"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <Page>
+        <ActionBar title="Calculator" class="action-bar"/>
+        <ScrollView>
+            <ActionButtons class="action-buttons"></ActionButtons>
+        </ScrollView>
+    </Page>
+</template>
+
+<script>
+    import ActionButtons from './ActionButtons';
+    export default {
+        components: {ActionButtons},
+        data() {
+            return {
+                msg: 'Peace)))'
+            }
+        },
+        methods: {
+            onButtonTap(num) {
+                console.log(num);
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+    .action-buttons {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
